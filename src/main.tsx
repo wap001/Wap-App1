f8a32f6e78884175998ca8098c1094f85256bbaa
wap001 <deleahwap@gmail.com>
2026-09-21 13:52:44 -0400

    Register service worker on load if supported

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+if ('serviceWorker' in navigator) { window.addEventListener('load', () => { navigator.serviceWorker.register('/sw.js'); }); }
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
